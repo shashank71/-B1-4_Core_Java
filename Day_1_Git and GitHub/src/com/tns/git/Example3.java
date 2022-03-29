0bde1c05823894be9505f20e8deed492dd3ecdba
shashank71 <53090271+shashank71@users.noreply.github.com>
2022-03-30 01:26:36 +0530

    First Commit

---
+++ Day_1_Git and GitHub/src/com/tns/git/Example3.java
@@ -0,0 +1,18 @@
+package com.tns.git;
+import java.util.Scanner;
+public class Example3 {
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int fact=1;
+		for(int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+		s.close();
+	}
+
+}
