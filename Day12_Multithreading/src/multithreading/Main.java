4e1e58edb3e7ccc0a8f309b846c9eb22917ab01d
shashank71 <53090271+shashank71@users.noreply.github.com>
2022-04-12 21:46:31 +0530

    First Commit

---
+++ Day12_Multithreading/src/multithreading/Main.java
@@ -0,0 +1,19 @@
+package multithreading;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		Thread t = Thread.currentThread( );
+		System.out.println("Current Thread :"+t);
+		t.setName("Void");
+		System.out.println("Current Thread :"+t);
+		try
+		{
+			Thread.sleep(1);
+		}
+		catch (InterruptedException e)
+		{
+			System.out.println("Main Thread Interrupted");
+		}
+	}
+}
