5fbb85facafe36f23648c15fe17649cbf28625cc
shashank71 <53090271+shashank71@users.noreply.github.com>
2022-04-14 18:43:23 +0530

    First Commit

---
+++ Day14_Strings/src/Strings/Question1.java
@@ -0,0 +1,22 @@
+package Strings;
+
+import java.util.Scanner;
+public class Question1
+{
+
+	public static void main(String[] args)
+	{
+		String str = "Dhoni";
+		Scanner s = new Scanner(System.in);
+		String str1 = s.nextLine();
+		if(str1.equals(str))
+		{
+			System.out.println("Matching");
+		}
+		else
+		{
+			System.out.println("Not Matching");
+		}
+		s.close();
+	}
+}
