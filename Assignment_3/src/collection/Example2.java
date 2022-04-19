ee16a2ccde4933ef9a9791cf6c42363f7511e34a
shashank71 <53090271+shashank71@users.noreply.github.com>
2022-04-19 16:18:46 +0530

    First Commit

---
+++ Assignment_3/src/collection/Example2.java
@@ -0,0 +1,19 @@
+package collection;
+
+import java.util.ArrayDeque;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayDeque a = new ArrayDeque();
+		a.add(11);
+		a.add("Hello");
+		System.out.println(a);
+		a.addFirst(12);
+		a.addLast("World");
+		System.out.println(a);
+		
+	}
+
+}
---
+++ Assignment_3/src/collection/Example5.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NavigableMap<Integer,String>s=new TreeMap<Integer,String>();
+		s.put(15, "soujan");
+		s.remove(12, null);
+		s.remove(12);
+		System.out.println(s);
+		s.firstEntry();
+	}
+
+}
