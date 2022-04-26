70cda96a592dc4f048887e6791cc6e83968060db
shashank71 <53090271+shashank71@users.noreply.github.com>
2022-04-26 09:09:03 +0530

    First Commit

---
+++ DAY20_Stream API/src/api/Example2.java
@@ -0,0 +1,15 @@
+package api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		List<Integer>values=Arrays.asList(11,22,33,22,60,25,11);
+		values.stream().filter(i->i<30).forEach(i->System.out.println(i));
+		values.stream().distinct().forEach(i->System.out.println("Unique values are" +i));
+		
+	}
+
+}
