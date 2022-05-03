c00395d801043ef360a43699e7c1e4182eeaf3bf
shashank71 <53090271+shashank71@users.noreply.github.com>
2022-05-03 09:11:06 +0530

    First Commit

---
+++ Day18_java_generics/src/generics/Example1.java
@@ -0,0 +1,23 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> a = new ArrayList<String>();
+		a.add("Soujan");
+		a.add("James");
+		String s=a.get(1);
+		System.out.println(s);
+		Iterator<String> itr=a.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+		//System.out.println(a);
+	}
+
+}
